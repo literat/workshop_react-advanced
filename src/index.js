dd32c8764894acbeb27c43bb4b7603cd6eb3b0d8
literat <tomas@litera.me>
2021-07-28 12:06:28 +0200

    feat: Create React App

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ul>
+      <li>
+        <div>React Context</div>
+        <div><a href="/exercises/01-context/context-START.js">Exercise</a></div>
+        <div><a href="/exercises/01-context/context-FINISHED.js">Finished</a></div>
+      </li>
+    </ul>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
