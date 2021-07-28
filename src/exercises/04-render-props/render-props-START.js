bb21ac0be82d4eed548513693759814e8ee3d8a1
literat <tomas@litera.me>
2021-07-28 10:15:00 +0200

    feat: Introduce render props

---
+++ src/exercises/04-render-props/render-props-START.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <img src="/cat.jpg" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
+    );
+  }
+}
+
+// The <Mouse> component encapsulates the behavior we need...
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+
+        {/* ...but how do we render something other than a <p>? */}
+        {/*
+          We could just swap out the <p> for a <Cat> here ... but then
+          we would need to create a separate <MouseWithSomethingElse>
+          component every time we need to use it, so <MouseWithCat>
+          isn't really reusable yet.
+        */}
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Move the mouse around!</h1>
+        <Mouse />
+      </>
+    );
+  }
+}
---
+++ src/exercises/04-render-props/render-props.md
@@ -0,0 +1,7 @@
+# Render Props
+
+<https://reactjs.org/docs/render-props.html>
+
+> The term “render prop” refers to a technique for sharing code between React components using a prop whose value is a function.
+
+Inversion of Control
